94e167b2eae2d30a81cc49bb6a0258774a33ff1f
Althasandrian <Althasandrian@gmail.com>
2014-11-09 00:21:04 +0200

    checkGLError lisätty debuggeriin
    Tällä voitte checkailla opengl errorit

--- ZzZ -engine/ZzZ -engine/debugger.h
+++ ZzZ -engine/ZzZ -engine/debugger.h
@@ -1,12 +1,11 @@
 #ifndef ZZZ_DEBUGGER
 #define ZZZ_DEBUGGER
-
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <time.h>
 #include <Windows.h>
-
+#include "GL\glew.h"
 /*
 HOW_TO?
 1. Use: Debugger::Print("your string here");
@@ -14,18 +13,16 @@ HOW_TO?
 3. ?????
 4. Profit!
 */
-
 class Debugger
 {
 public:
 	static Debugger* Instance();
 	static void Print(std::string debugString);
+	static void checkGLError(std::string debugString);
 protected:
 	Debugger();
 	~Debugger();
 private:
 	static Debugger* instance;
 };
-
-
 #endif // !ZZZ_DEBUGGER
