a5b4c2ed7d03c791f1cc76612d6e42d45fab6bce
Althasandrian <Althasandrian@gmail.com>
2014-12-05 10:57:27 +0200

    scale rikki rotaten kans

--- ZzZ -engine/ZzZ -engine/main.cpp
+++ ZzZ -engine/ZzZ -engine/main.cpp
@@ -28,91 +28,133 @@ int main()
 
 	Timer timer = Timer(millisecond);
 	
-	Texture testiTex("lode_test_in");
-	Sprite sprite1(&testiTex, 300, 450);
+	Texture zzzTexture("lode_test_in");
+	Texture koalaTexture("koala");
+	Texture boobs("boobs");
+	Texture explosions("explosions");
+
+	Sprite sprite1(&zzzTexture, 300, 450);
+	sprite1.setLayerDepth(2);
+	std::vector<Sprite*> koalaFarmi;
 
 	float angle = 0;
+	float move = 0;
+	float scale = 1;
 	while (testi.isRunning)
 	{
 		//start timer
 		timer.startDeltaTime();
-		testi.input.update();
 
-		angle += 0.01 * timer.getDeltaTime();
-		if (angle > 360)
-			angle -= 360;
-		sprite1.setRotation(angle);
-
+		angle = 0.03 * timer.getDeltaTime();
+		move = 0.1 * timer.getDeltaTime();
+		
 		//Move Up
-		if (testi.input.isKeyPressed(Key::W))
+		if (testi.input.isKeyDown(Key::W))
 		{
-
+			sprite1.moveSprite(0.0f, -move);
 		}
 
 		//Move Left
-		if (testi.input.isKeyPressed(Key::A))
+		if (testi.input.isKeyDown(Key::A))
 		{
-
+			sprite1.moveSprite(-move, 0.0f);
 		}
 
 		//Move Down
-		if (testi.input.isKeyPressed(Key::S))
+		if (testi.input.isKeyDown(Key::S))
 		{
-
+			sprite1.moveSprite(0.0f, move);
 		}
 
 		//Move Right
-		if (testi.input.isKeyPressed(Key::D))
+		if (testi.input.isKeyDown(Key::D))
 		{
-
+			sprite1.moveSprite(move, 0.0f);
 		}
 
 		//Rotate Counterclockwise
-		if (testi.input.isKeyPressed(Key::Q))
+		if (testi.input.isKeyDown(Key::Q))
 		{
-
+			sprite1.rotate(angle);
 		}
 
 		//Rotate Clockwise
-		if (testi.input.isKeyPressed(Key::E))
+		if (testi.input.isKeyDown(Key::E))
 		{
-
+			sprite1.rotate(-angle);
 		}
 
 		//Scale Up
-		if (testi.input.isKeyPressed(Key::R))
+		if (testi.input.isKeyDown(Key::R))
 		{
-
+			scale += 0.01;
+			sprite1.setScale(scale);
 		}
 
 		//Scale Down
-		if (testi.input.isKeyPressed(Key::F))
+		if (testi.input.isKeyDown(Key::F))
 		{
-
+			scale -= 0.01;
+			sprite1.setScale(scale);
 		}
 
 		//Previous Texture
 		if (testi.input.isKeyPressed(Key::Z))
 		{
-
+			if (sprite1.getTexture() == &zzzTexture)
+			{
+				sprite1.setTexture(&boobs);
+			}
+			else if (sprite1.getTexture() == &boobs)
+			{
+				sprite1.setTexture(&explosions);
+			}
+			else if (sprite1.getTexture() == &explosions)
+			{
+				sprite1.setTexture(&zzzTexture);
+			}
 		}
 
 		//Next Texture
 		if (testi.input.isKeyPressed(Key::X))
 		{
-
+			if (sprite1.getTexture() == &zzzTexture)
+			{
+				sprite1.setTexture(&explosions);
+			}
+			else if (sprite1.getTexture() == &boobs)
+			{
+				sprite1.setTexture(&zzzTexture);
+			}
+			else if (sprite1.getTexture() == &explosions)
+			{
+				sprite1.setTexture(&boobs);
+			}
 		}
 
 		//Spawn Koala
 		if (testi.input.isKeyPressed(Key::C))
 		{
-
+			int x = std::rand() % 600;
+			int y = std::rand() % 800;
+			Sprite* koala = new Sprite(&koalaTexture, 128, 128);
+			koala->setPosition(x, y);
+			koalaFarmi.push_back(koala);
 		}
 
 		//Despawn Koala
 		if (testi.input.isKeyPressed(Key::MouseLeft))
 		{
-
+			int dii = 0;
+			for (std::vector<Sprite*>::iterator i = koalaFarmi.begin(); i < koalaFarmi.end(); i++)
+			{
+				if (koalaFarmi[dii]->contains(testi.input.getCursorPos().x, testi.input.getCursorPos().y))
+				{
+					Debugger::Print("Ny");
+					delete koalaFarmi.at(dii);
+				}
+				dii++;
+			}
 		}
 
 
@@ -126,10 +168,7 @@ int main()
 
 
 
-
-
-
-
+		testi.input.update();
 		//Do-things
 		testi.update();
 		timer.setDeltaTime();
